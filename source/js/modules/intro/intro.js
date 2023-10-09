0c4b8b24fbceb590563b46723fcca412c4c6243f
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-09 15:28:14 +0600

    fix errors editorconfig, stylelint

--- source/js/modules/intro/intro.js
+++ source/js/modules/intro/intro.js
@@ -5,18 +5,6 @@ const intro = () => {
   introBtn.addEventListener('click', () => {
     contact.scrollIntoView({behavior: "smooth"});
   })
-
-  // const changeTextIntroBtn = () => {
-  //   if (window.matchMedia("(max-width: 767px)").matches) {
-  //     introBtn.textContent = 'Бесплатная консультация';
-  //   } else {
-  //     introBtn.textContent = 'Получить бесплатную консультацию';
-  //   }
-  // }
-
-  // changeTextIntroBtn();
-
-  // window.addEventListener('resize', changeTextIntroBtn);
 };
 
 export { intro };
