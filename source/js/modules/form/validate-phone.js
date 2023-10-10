63c014b0dc42a86fe66f327d713ad4cc60b5b35a
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-10 14:17:45 +0600

    add mask phone

--- source/js/modules/form/validate-phone.js
+++ source/js/modules/form/validate-phone.js
@@ -1,36 +1,36 @@
 const validatePhone = () => {
 
   [].forEach.call(document.querySelectorAll('.custom-input--phone'), function (input) {
-    var keyCode;
+    let keyCode;
     function mask(event) {
       event.keyCode && (keyCode = event.keyCode);
-      var pos = this.selectionStart;
+      let pos = this.selectionStart;
       if (pos < 3) event.preventDefault();
-      var matrix = "+7 (___) ___ ____",
+      let matrix = '+7 (___) ___ ____',
         i = 0,
-        def = matrix.replace(/\D/g, ""),
-        val = this.value.replace(/\D/g, ""),
+        def = matrix.replace(/\D/g, ''),
+        val = this.value.replace(/\D/g, ''),
         new_value = matrix.replace(/[_\d]/g, function (a) {
           return i < val.length ? val.charAt(i++) || def.charAt(i) : a
         });
-      i = new_value.indexOf("_");
+      i = new_value.indexOf('_');
       if (i != -1) {
         i < 5 && (i = 3);
         new_value = new_value.slice(0, i)
       }
-      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+      let reg = matrix.substr(0, this.value.length).replace(/_+/g,
         function (a) {
-          return "\\d{1," + a.length + "}"
-        }).replace(/[+()]/g, "\\$&");
-      reg = new RegExp("^" + reg + "$");
+          return '\\d{1,' + a.length + '}'
+        }).replace(/[+()]/g, '\\$&');
+      reg = new RegExp('^' + reg + '$');
       if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-      if (event.type == "blur" && this.value.length < 5) this.value = ""
+      if (event.type == 'blur' && this.value.length < 5) this.value = ''
     }
 
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-    input.addEventListener("keydown", mask, false);
+    input.addEventListener('input', mask, false);
+    input.addEventListener('focus', mask, false);
+    input.addEventListener('blur', mask, false);
+    input.addEventListener('keydown', mask, false);
 
   });
 };
