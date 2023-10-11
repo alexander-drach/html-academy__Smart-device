1540cd81526bcaf4a70b4b303f365df571c2ee96
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-11 20:44:12 +0600

    add scroll behavior

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,6 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {accordeon} from './modules/accordeon/accordeon';
-import {intro} from './modules/intro/intro';
 import {showFullTextAbout} from './modules/about/show-full-text-about';
 
 // ---------------------------------
@@ -14,7 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   iosVhFix();
   accordeon();
-  intro();
   showFullTextAbout();
 
   // Modules
